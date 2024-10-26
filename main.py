838257312413d4486cc32fedd57b2a9df54911bd
Logan John Owens <loganjohnowens@users.noreply.github.com>
2024-10-25 19:02:53 -0600

    fix screen not displaying

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import pygame
-screen_1 = 0
+pygame.init()
+
 distance = 50
 screen_size = 500
-num_blocks = screen_size/distance
-pygame.init()
+num_blocks = screen_size // distance
+
 screen = pygame.display.set_mode((screen_size, screen_size))
-h =input ('is the display here')
 pygame.display.set_caption("snake")
-screen.fill((0, 0, 0))
-snake = [[num_blocks / 2 ,num_blocks / 2]]
-while True:
-    for j in range(1, (len(snake)-1)):
-        for i in snake[j][0]:
-           pygame.draw.rect(screen, (255, 255, 255), (distance*i, distance*i, distance, distance))
-        for i in snake[j][1]:
-            pygame.draw.rect(screen, (255, 255, 255), (distance*i, distance*i, distance, distance))
-        print('hi')
+
+snake = [[num_blocks // 2, num_blocks // 2]]
+
+running = True
+while running:
+    screen.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    for segment in snake:
+        x, y = segment
+        pygame.draw.rect(screen, (255, 255, 255),
+                         (x * distance, y * distance, distance, distance))
+
     pygame.display.flip()
-    
+
+pygame.quit()
