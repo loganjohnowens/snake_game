731b297d3f368082264dee3be80b205f5f2dd6b3
Logan John Owens <loganjohnowens@users.noreply.github.com>
2024-11-07 16:44:06 -0700

    added_faulty_AI

--- main.py
+++ main.py
@@ -10,13 +10,17 @@ num_blocks = screen_size // distance
 screen = pygame.display.set_mode((screen_size, screen_size))
 pygame.display.set_caption("Snake Game")
 
-snake = [[num_blocks // 2 - 5, num_blocks // 2]]
+snake = [[num_blocks // 2 - 3, num_blocks // 2 - 1]]
 apple_get = 0
 apple_loc_x = (num_blocks // 2 + 2)
 apple_loc_y = (num_blocks // 2)
 move_state = [False] * 323
 move_state[pygame.K_d] = True
 diecoin = pygame.K_d
+mode = input('''what mode would you like to play:
+        to watch ai press 1
+        to play press 2
+        >''')
 
 
 def move(second_loop, diecoin):
@@ -54,25 +58,24 @@ def apple():
     global apple_loc_y
     global snake
     if apple_get == 1:
-        apple_loc_x = random.randint(0, num_blocks - 1)
-        apple_loc_y = random.randint(0, num_blocks - 1)
         apple_get = 0
     if snake[0] == [apple_loc_x, apple_loc_y]:
         apple_get = 1
+        apple_loc_x = random.randint(0, num_blocks - 1)
+        apple_loc_y = random.randint(0, num_blocks - 1)
 
 
-def snake_run_in():
+def snake_run_in(head):
     global snake
     number = 0
     for i in snake:
-        if i == snake[0] and number == 1:
+        if i == head and number == 1:
             return False
         number = 1
     return True
 
 
-def walls():
-    global snake
+def walls(snake):
     global num_blocks
     if snake[0][0] < 0:
         return False
@@ -85,23 +88,132 @@ def walls():
     return True
 
 
+def bot_move():
+    global snake
+    global apple_loc_y
+    global apple_loc_x
+    global apple_get
+    print(apple_loc_x, apple_loc_y)
+    new_potential_loc = []
+    potential_loc = [[snake[0][0] + 1, snake[0][1]], [snake[0][0] - 1, snake[0][1]],
+                     [snake[0][0], snake[0][1] + 1], [snake[0][0], snake[0][1] - 1]]
+    for i in potential_loc:
+        check = []
+        check.append(i)
+        check.append(['why are you reading this'])
+        new_potential_loc.append(walls(check))
+        new_potential_loc.append(snake_run_in(i))
+    if new_potential_loc[0] is False or new_potential_loc[4] is False:
+        new_potential_loc[0] = False
+    if new_potential_loc[1] is False or new_potential_loc[5] is False:
+        new_potential_loc[1] = False
+    if new_potential_loc[2] is False or new_potential_loc[6] is False:
+        new_potential_loc[2] = False
+    if new_potential_loc[3] is False or new_potential_loc[7] is False:
+        new_potential_loc[3] = False
+    new_potential_loc = new_potential_loc[:-4]
+    best_number = 0
+    done = 0
+    super_done = 0
+    if potential_loc[0][0] == apple_loc_x and apple_loc_y == potential_loc[0][1]:
+        super_done = 1
+        best_number = 0
+        done = 1
+    if potential_loc[1][0] == apple_loc_x and apple_loc_y == potential_loc[1][1]:
+        super_done = 1
+        best_number = 1
+        done = 1
+    if potential_loc[2][0] == apple_loc_x and apple_loc_y == potential_loc[2][1]:
+        super_done = 1
+        best_number = 2
+        done = 1
+    if potential_loc[3][0] == apple_loc_x and apple_loc_y == potential_loc[3][1]:
+        super_done = 1
+        best_number = 3
+        done = 1
+    numbers = -1
+    for i in potential_loc:
+        numbers += 1
+        if i[0] == apple_loc_x and numbers < 2:
+            best_number = numbers
+            super_done = 1
+            done = 1
+        if i[1] == apple_loc_y and numbers > 1:
+            best_number = numbers
+            super_done = 1
+            done = 1
+    print(super_done)
+    if done != 1 and potential_loc[0][0] < apple_loc_x:
+        potential_loc[0] = apple_loc_x - potential_loc[0][0]
+        done = 1
+    if done != 1 and potential_loc[0][0] > apple_loc_x:
+        potential_loc[0] = potential_loc[0][0] - apple_loc_x
+    if super_done != 1:
+        done = 0
+    if done != 1 and potential_loc[1][0] < apple_loc_x:
+        potential_loc[1] = apple_loc_x - potential_loc[1][0]
+        done = 1
+    if done != 1 and potential_loc[1][0] > apple_loc_x:
+        potential_loc[1] = potential_loc[1][0] - apple_loc_x
+    if super_done != 1:
+        done = 0
+    if done != 1 and potential_loc[2][1] < apple_loc_y:
+        potential_loc[2] = apple_loc_y - potential_loc[2][1]
+        done = 1
+    if done != 1 and potential_loc[2][1] > apple_loc_y:
+        potential_loc[2] = potential_loc[2][1] - apple_loc_y
+    if super_done != 1:
+        done = 0
+    if done != 1 and potential_loc[3][1] < apple_loc_y:
+        potential_loc[3] = apple_loc_y - potential_loc[3][1]
+        done = 1
+    if done != 1 and potential_loc[3][1] > apple_loc_y:
+        potential_loc[3] = potential_loc[3][1] - apple_loc_y
+    best = 1000
+    number = -1
+    if super_done != 1:
+        print(potential_loc)
+        for i in potential_loc:
+            number += 1
+            if i < best and new_potential_loc[number] is True:
+                if number < 2 and snake[0][0] != apple_loc_x:
+                    best = i
+                    best_number = number
+                if number > 1 and snake[0][1] != apple_loc_y:
+                    best = i
+                    best_number = number
+    if best_number == 0:
+        snake.insert(0, [snake[0][0] + 1, snake[0][1]])
+    if best_number == 1:
+        snake.insert(0, [snake[0][0] - 1, snake[0][1]])
+    if best_number == 2:
+        snake.insert(0, [snake[0][0], snake[0][1] + 1])
+    if best_number == 3:
+        snake.insert(0, [snake[0][0], snake[0][1] - 1])
+    if apple_get == 0:
+        snake.pop()
+
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill((0, 0, 0))
-    diecoin = move(False, diecoin)
+    if mode == '2':
+        diecoin = move(False, diecoin)
+        apple()
     pygame.draw.rect(screen, (225, 0, 0),
                      (apple_loc_x * distance, apple_loc_y * distance, distance - 1, distance - 1))
-
+    if mode == '1':
+        bot_move()
+        apple()
     for segment in snake:
         x, y = segment
         pygame.draw.rect(screen, (0, 225, 0), (x * distance,
                          y * distance, distance - 1, distance - 1))
-    apple()
-    running_one = snake_run_in()
-    running = walls()
+    running_one = snake_run_in(snake[0])
+    running = walls(snake)
     if running_one is False:
         running = False
     pygame.display.flip()
